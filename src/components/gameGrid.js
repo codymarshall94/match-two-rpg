32ee2e76516b9b3f7553c4b6ba748ae3e4ebaf14
Cody Marshall <codymstuntsx@gmail.com>
2022-07-11 14:07:17 -0700

    first commit

---
+++ src/components/gameGrid.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+function GameGrid() {
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col grid-item">Column 1</div>
+        <div class="col">Column 2 </div>
+        <div class="col">Column 3</div>
+        <div class="col">Column 4</div>
+        <div class="col">Column 5</div>
+      </div>
+      <div class="row">
+        <div class="col">Column 6</div>
+        <div class="col">Column 7 </div>
+        <div class="col">Column 8</div>
+        <div class="col">Column 9</div>
+        <div class="col">Column 10</div>
+      </div>
+      <div class="row">
+        <div class="col">Column 11</div>
+        <div class="col">Column 12 </div>
+        <div class="col">Column 13</div>
+        <div class="col">Column 14</div>
+        <div class="col">Column 15</div>
+      </div>
+      <div class="row">
+        <div class="col">Column 16</div>
+        <div class="col">Column 17</div>
+        <div class="col">Column 18</div>
+        <div class="col">Column 19</div>
+        <div class="col">Column 20</div>
+      </div>
+    </div>
+  );
+}
+
+export default GameGrid;
