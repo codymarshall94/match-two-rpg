5d429489f41edc09c2540e87f6171baf394707ab
Cody Marshall <codymstuntsx@gmail.com>
2022-07-11 17:52:51 -0700

    card component created

---
+++ src/components/SingleCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SingleCard({handleCardClick, cardsFlipped, cardsCorrect, cards}) {
+
+  return (
+    <div className="row">
+      {cards.map((card, index) => (
+        <div key={index} className="col-4 card" onClick={() => handleCardClick(index)}>
+          {cardsFlipped.includes(index) || cardsCorrect.includes(index) ? <img className="card-img" src={card.front} alt="card front"/> : <img className="card-img" src={require("../card-fronts/card-random.png")} />}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default SingleCard;
