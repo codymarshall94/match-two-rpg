e1a4022d4ccdab3cf67d9f071ab6ead31751d143
Cody Marshall <codymstuntsx@gmail.com>
2022-07-11 17:52:39 -0700

    removed static columns and added dynamic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+//import { useState } from 'react';
 import './App.css';
 import GameGrid from './components/gameGrid';
 
+//start game with all cards flipped upside down
+//allow player to select a card and flip upright
+//select a second card and check to see if they match
+// if they dont match, flip both back down, if they do match they remain up
+
 function App() {
+  
+
   return (
     <div className="App">
      <GameGrid />
