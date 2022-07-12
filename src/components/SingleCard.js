773c6830b5274d39cfad1f51f8990b5e186aa330
Cody Marshall <codymstuntsx@gmail.com>
2022-07-12 12:45:22 -0700

    added ternarys for images

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,23 @@
 import React from "react";
 
-function SingleCard({handleCardClick, cardsFlipped, cardsCorrect, cards}) {
-
+function SingleCard({ handleCardClick, cardsFlipped, cardsCorrect, cards }) {
   return (
     <div className="row">
       {cards.map((card, index) => (
-        <div key={index} className="col-4 card" onClick={() => handleCardClick(index)}>
-          {cardsFlipped.includes(index) || cardsCorrect.includes(index) ? <img className="card-img" src={card.front} alt="card front"/> : <img className="card-img" src={require("../card-fronts/card-random.png")} />}
+        <div
+          key={index}
+          className="col-4 card"
+          onClick={() => handleCardClick(index)}
+        >
+          {cardsFlipped.includes(index) || cardsCorrect.includes(index) ? (
+            <img className="card-img" src={card.front} alt="card front" />
+          ) : (
+            <img
+              className="card-img"
+              src={require("../card-fronts/card-random.png")}
+              alt="card back"
+            />
+          )}
         </div>
       ))}
     </div>
