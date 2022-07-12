0fc0dafadbc7bbba84c48d362c40a4621e7e8fec
Cody Marshall <codymstuntsx@gmail.com>
2022-07-11 17:53:10 -0700

    removed static columns

--- src/components/gameGrid.js
+++ src/components/gameGrid.js
@@ -1,38 +1,48 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import "../css/gamegrid.css";
+import { CARDS } from "../cards";
+import SingleCard from "./SingleCard";
 
 function GameGrid() {
-  return (
-    <div class="container">
-      <div class="row">
-        <div class="col grid-item">Column 1</div>
-        <div class="col">Column 2 </div>
-        <div class="col">Column 3</div>
-        <div class="col">Column 4</div>
-        <div class="col">Column 5</div>
-      </div>
-      <div class="row">
-        <div class="col">Column 6</div>
-        <div class="col">Column 7 </div>
-        <div class="col">Column 8</div>
-        <div class="col">Column 9</div>
-        <div class="col">Column 10</div>
-      </div>
-      <div class="row">
-        <div class="col">Column 11</div>
-        <div class="col">Column 12 </div>
-        <div class="col">Column 13</div>
-        <div class="col">Column 14</div>
-        <div class="col">Column 15</div>
-      </div>
-      <div class="row">
-        <div class="col">Column 16</div>
-        <div class="col">Column 17</div>
-        <div class="col">Column 18</div>
-        <div class="col">Column 19</div>
-        <div class="col">Column 20</div>
-      </div>
-    </div>
-  );
+const [cards, setCards] = useState(CARDS.concat(CARDS).sort(() => Math.random() - 0.5));
+const [choiceOne, setChoiceOne] = useState(null);
+const [choiceTwo, setChoiceTwo] = useState(null);
+const [cardsFlipped, setCardsFlipped] = useState([]);
+const [cardsCorrect, setCardsCorrect] = useState([]);
+
+const handleCardClick = (cardIndex) => {
+    setCardsFlipped((prev) => [...prev, cardIndex]);
+}
+
+const evaluate = () => {
+    const [first, second] = cardsFlipped;
+    if(cards[first].name === cards[second].name) { //using the destructured array as a way of searching cards array
+        setCardsCorrect((prev) => [...prev, first, second]);
+        setCardsFlipped([]);
+    } else {
+        setCardsFlipped([]);
+    }
+    checkCompletion(); 
+}
+
+//checking completion everytime a pair is evaluated
+const checkCompletion = () => { 
+    if (cards.length === cardsCorrect.length) {
+      alert("you win");
+    }
+  };
+
+useEffect(() => {
+    if(cardsFlipped.length === 2) {
+        setTimeout(evaluate, 500);
+    }
+}, [cardsFlipped])
+
+return (
+<div className="container game-grid border">
+    <SingleCard handleCardClick={handleCardClick} cardsFlipped={cardsFlipped} cardsCorrect={cardsCorrect} cards={cards}/>
+</div>
+);
 }
 
 export default GameGrid;
