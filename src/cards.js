206a5156c4aa2c58e6c8fc973b1f509372b4179b
Cody Marshall <codymstuntsx@gmail.com>
2022-07-11 17:52:07 -0700

    array of cards

---
+++ src/cards.js
@@ -0,0 +1,55 @@
+export const CARDS = [
+    {
+        id: 1,
+        name: "strength up",
+        front: require("./card-fronts/warlord-helmet.png"),
+        
+    },
+    {
+        id: 2,
+        name: "health up",
+        front: require("./card-fronts/glass-heart.png"),
+    },
+    {
+        id: 3,
+        name: "attack three",
+        front: require("./card-fronts/haunting.png"),
+    },
+    {
+        id: 4,
+        name: "attack four",
+        front: require("./card-fronts/vile-fluid.png"),
+    },
+    {
+        id: 5,
+        name: "attack five",
+        front: require("./card-fronts/rally-the-troops.png"),
+    },
+    {
+        id: 6,
+        name: "attack six",
+        front: require("./card-fronts/beanstalk.png"),
+    },
+    {
+        id: 7,
+        name: "attack seven",
+        front: require("./card-fronts/deadly-strike.png"),
+    },
+    {
+        id: 8,
+        name: "attack eight",
+        front: require("./card-fronts/embrassed-energy.png"),
+    },
+    {
+        id: 9,
+        name: "attack nine",
+        front: require("./card-fronts/cold-heart.png"),
+    },
+    {
+        id: 10,
+        name: "attack ten",
+        front: require("./card-fronts/death-note.png"),
+    },
+    
+
+]
