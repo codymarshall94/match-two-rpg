4c3e07e85cccfb6d8f395b0e1e31445ecdbf1a08
Cody Marshall <codymstuntsx@gmail.com>
2022-07-13 16:08:15 -0700

    format

--- src/components/CombatLog.js
+++ src/components/CombatLog.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 function CombatLog({cardsCorrect, cards}) {
 let currentEffect = cardsCorrect[cardsCorrect.length - 1];
-console.log(currentEffect);
-
   return (
     <div className='log-box'>
         {cardsCorrect && currentEffect ? <span className='combat-text'>{cards[currentEffect].effect.charAt(0).toUpperCase() + cards[currentEffect].effect.slice(1)}</span> : null}
