dd8bdd71b829bad70badcdd1da3f4d270e1bb971
Cody Marshall <codymstuntsx@gmail.com>
2022-07-12 20:38:19 -0700

    added animation for cardflip

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,26 +1,65 @@
 import React from "react";
 
-function SingleCard({ handleCardClick, cardsFlipped, cardsCorrect, cards }) {
+const RenderCard = ({ card, cardsFlipped, cardsCorrect, index }) => {
   return (
-    <div className="row">
+    <div className={
+      cardsFlipped.includes(index) || cardsCorrect.includes(index) ? "card-tile" : "flip-card card-tile"
+    }>
+      <div class="flip-card-inner">
+      <div class="flip-card-back">
+          <img
+            className="card-img"
+            src={require("../card-fronts/card-random.png")}
+            alt="card back"
+          />
+        </div>
+        <div class="flip-card-front">
+          <img className="card-img" src={card.front} alt="Avatar" />
+        </div>
+      </div>
+    </div>
+
+    //backup plan
+
+    /*
+    <div
+      className={
+        cardsFlipped.includes(index) ? "card-tile card-flip" : "card-tile"
+      }
+    >
+      {cardsFlipped.includes(index) || cardsCorrect.includes(index) ? (
+        <img className="card-img" src={card.front} alt="card front" />
+      ) : (
+        <img
+          className="card-img"
+          src={require("../card-fronts/card-random.png")}
+          alt="card back"
+        />
+      )}
+    </div> */
+  );
+};
+
+function SingleCard({
+  handleCardClick,
+  cards,
+  cardsFlipped,
+  cardsCorrect,
+  index,
+}) {
+  return (
+    <>
       {cards.map((card, index) => (
-        <div
-          key={index}
-          className="col-4 card"
-          onClick={() => handleCardClick(index)}
-        >
-          {cardsFlipped.includes(index) || cardsCorrect.includes(index) ? (
-            <img className="card-img" src={card.front} alt="card front" />
-          ) : (
-            <img
-              className="card-img"
-              src={require("../card-fronts/card-random.png")}
-              alt="card back"
-            />
-          )}
+        <div key={index} onClick={() => handleCardClick(index)}>
+          <RenderCard
+            card={card}
+            cardsFlipped={cardsFlipped}
+            cardsCorrect={cardsCorrect}
+            index={index}
+          />
         </div>
       ))}
-    </div>
+    </>
   );
 }
 
