3d84b4b1d96341efa1c974058f77aee5d362c59d
Cody Marshall <codymstuntsx@gmail.com>
2022-07-13 12:10:40 -0700

    updated cards with effects

--- src/cards.js
+++ src/cards.js
@@ -1,54 +1,63 @@
 export const CARDS = [
     {
         id: 1,
-        name: "strength up",
+        name: "brute force",
         front: require("./card-fronts/warlord-helmet.png"),
-        
+        effect: "a basic attack dealing 1 damage",
     },
     {
         id: 2,
         name: "health up",
         front: require("./card-fronts/glass-heart.png"),
+        effect: "recover 3hp",
     },
     {
         id: 3,
-        name: "attack three",
+        name: "haunting",
         front: require("./card-fronts/haunting.png"),
+        effect: "deal 4 damage but you skip next turn"
     },
     {
         id: 4,
-        name: "attack four",
+        name: "vile water",
         front: require("./card-fronts/vile-fluid.png"),
+        effect: "you drink the water and lose 2 hp",
     },
     {
         id: 5,
-        name: "attack five",
+        name: "rally the troops",
         front: require("./card-fronts/rally-the-troops.png"),
+        effect: "gain another turn"
     },
     {
         id: 6,
-        name: "attack six",
+        name: "root",
         front: require("./card-fronts/beanstalk.png"),
+        effect: "roots enemy for 2 turns dealing 1 damage each turn"
     },
     {
         id: 7,
-        name: "attack seven",
+        name: "deadly strike",
         front: require("./card-fronts/deadly-strike.png"),
+        effect: "enemy takes 3 damage",
     },
     {
         id: 8,
-        name: "attack eight",
+        name: "empowered attack",
         front: require("./card-fronts/embrassed-energy.png"),
+        effect: "doubles the next attack",
     },
     {
         id: 9,
-        name: "attack nine",
+        name: "cold heart",
         front: require("./card-fronts/cold-heart.png"),
+        effect: "freeze the enemy for 2 turns",
     },
     {
         id: 10,
-        name: "attack ten",
+        name: "death note",
         front: require("./card-fronts/death-note.png"),
+        effect: "enemy takes a randomized damage",
     },
     
 
