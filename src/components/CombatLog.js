16b954b29f37d7d039c75da44e421ccbf7395e91
Cody Marshall <codymstuntsx@gmail.com>
2022-07-15 16:04:46 -0700

    added conditionals for setting the effect

--- src/components/CombatLog.js
+++ src/components/CombatLog.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from 'react';
+
 
 function CombatLog({cardsCorrect, cards}) {
-let currentEffect = cardsCorrect[cardsCorrect.length - 1];
+  let currentEffect;
+
+  if(cardsCorrect.length >= 1) {
+    currentEffect = cardsCorrect[cardsCorrect.length - 1].cardItem.card.effect || cardsCorrect[cardsCorrect.length - 1].cardItem.enemyFirst.effect;
+  }
+  
   return (
     <div className='log-box'>
-        {cardsCorrect && currentEffect ? <span className='combat-text'>{cards[currentEffect].effect.charAt(0).toUpperCase() + cards[currentEffect].effect.slice(1)}</span> : null}
+        {currentEffect !== undefined ? <span className='combat-text'>{currentEffect}</span> : null}
     </div>
   )
 }
+
+//{cards[currentEffect].cardItem.card.effect.charAt(0).toUpperCase() + cards[currentEffect].cardItem.card.effect.slice(1)}
 
 export default CombatLog
