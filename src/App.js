31d46f3e9654b89d647d8dc57f154eb6a364958c
Cody Marshall <codymstuntsx@gmail.com>
2022-07-15 16:04:05 -0700

    reformatted code

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
     CARDS.concat(CARDS).sort(() => Math.random() - 0.5)
   );
   const [cardsFlipped, setCardsFlipped] = useState([]);
+  const [selectedCardIndexes, setSelectedCardIndexes] = useState([]); //using this to easily get indexes to flip cards
+  const [correctCardIndexes, setCorrectCardIndexes] = useState([]); //same reason as selectedCardIndexes
   const [cardsCorrect, setCardsCorrect] = useState([]);
   const [playerTurn, setPlayerTurn] = useState(true);
   const [turn, setTurn] = useState(1);
@@ -25,60 +27,75 @@ function App() {
   const timeout = useRef(null);
   let damageTaken;
 
-  const handleCardClick = (cardIndex) => {
-    // Have a maximum of 2 items in array at once.
-    if (cardsFlipped.length === 1) {
-      setCardsFlipped((prev) => [...prev, cardIndex]);
-    } else {
-      // If two cards are already open, we cancel timeout set for flipping cards back
-      clearTimeout(timeout.current);
-      setCardsFlipped([cardIndex]);
+  const handleCardClick = (cardItem) => {
+    setCardsFlipped((prev) => [...prev, { cardItem }]);
+    setSelectedCardIndexes((prev) => [...prev, cardItem.index]);
+  };
+
+  useEffect(() => {
+    if (cardsFlipped.length === 2) {
+      setTimeout(evaluate, 500);
     }
+    // eslint-disable-next-line
+  }, [cardsFlipped]);
+
+  //everytime 2 cards are flipped they are evaluated
+  const evaluate = () => {
+    const [first, second] = cardsFlipped; //using the destructured array as a way of searching cards array
+    console.log(first);
+    if (
+      cards[first.cardItem.index].name === cards[second.cardItem.index].name
+    ) {
+      setCardsCorrect((prev) => [...prev, first, second]);
+      setCorrectCardIndexes((prev) => [
+        ...prev,
+        first.cardItem.index,
+        second.cardItem.index,
+      ]);
+      dealDamage();
+    }
+    timeout.current = setTimeout(() => {
+      setCardsFlipped([]);
+      setSelectedCardIndexes([]);
+    }, 500);
+    checkCompletion();
+    changeTurn();
   };
 
   //setting the turn between enemy and player
   const changeTurn = () => {
     setTimeout(() => {
       setTurn(turn + 1);
-      turn % 2 === 0 ? setPlayerTurn(true) : setPlayerTurn(false); 
+      turn % 2 === 0 ? setPlayerTurn(true) : setPlayerTurn(false);
     }, 500);
-    if(playerTurn) { //currently working with playerTurn set to positive. need to figure out why
-      setTimeout(enemyTurn , 1000);
+    if (playerTurn) {
+      //currently working with playerTurn set to positive. need to figure out why
+      setTimeout(enemyTurn, 1000);
     }
-  }
+  };
 
   const enemyTurn = () => {
+    const randomIndex = Math.floor(Math.random() * (cards.length - 0) + 0);
+    const randomIndexTwo = Math.floor(Math.random() * (cards.length - 0) + 0);
     setTimeout(() => {
-      setCardsFlipped((prev) => [...prev, Math.floor(Math.random() * (cards.length - 0) + 0)]);//using delay on setting cards flipped for a delayed flip
-    },500);
+      const enemyFirst = cards[randomIndex];
+      handleCardClick({ card: enemyFirst, index: randomIndex });
+    }, 500);
     setTimeout(() => {
-      setCardsFlipped((prev) => [...prev, Math.floor(Math.random() * (cards.length - 0) + 0)]);
-    },1500);
-  }
+      const enemySecond = cards[randomIndexTwo];
+      handleCardClick({ card: enemySecond, index: randomIndexTwo });
+    }, 1500);
+  };
 
-
+  //function will only be called if a match is made
   const dealDamage = () => {
-    damageTaken = cards[cardsCorrect[cardsCorrect.length -1]].damage;
-    if(playerTurn) {
+    damageTaken = cardsCorrect[cardsCorrect.length - 1].cardItem.card.damage;
+    console.log(damageTaken);
+    if (playerTurn) {
       setEnemyHealth(enemyHealth - damageTaken);
     } else {
       setPlayerHealth(playerHealth - damageTaken);
     }
-  }
-
-
-  //everytime 2 cards are flipped they are evaluated
-  const evaluate = () => {
-    const [first, second] = cardsFlipped; //using the destructured array as a way of searching cards array
-    if (cards[first].name === cards[second].name) {
-      setCardsCorrect((prev) => [...prev, first, second]);
-      dealDamage();
-    }
-    timeout.current = setTimeout(() => {
-      setCardsFlipped([]);
-    }, 500);
-    checkCompletion();
-    changeTurn();
   };
 
   //checking completion everytime a pair is evaluated
@@ -90,20 +107,15 @@ function App() {
 
   const resetCards = () => {
     setCards(null);
-  }
-
-  useEffect(() => {
-    if (cardsFlipped.length === 2) {
-      setTimeout(evaluate, 500);
-    }
-  // eslint-disable-next-line
-  }, [cardsFlipped]);
+  };
 
   return (
     <div className="app-container">
-      <CombatLog cardsCorrect={cardsCorrect} cards={cards}/>
+      <CombatLog cardsCorrect={cardsCorrect} cards={cards} />
       <div className="card-grid">
         <SingleCard
+          selectedCardIndexes={selectedCardIndexes}
+          correctCardIndexes={correctCardIndexes}
           playerTurn={playerTurn}
           handleCardClick={handleCardClick}
           cardsFlipped={cardsFlipped}
@@ -111,10 +123,16 @@ function App() {
           cards={cards}
         />
       </div>
-      <Moves turn={turn}/>
-      <div className="current-turn">{playerTurn === true ? <span>Player Turn</span> : <span>Enemy Turn</span>}</div>
-      <PlayerDisplay playerHealth={playerHealth}/>
-      <EnemyDisplay enemyHealth={enemyHealth}/>
+      <Moves turn={turn} />
+      <div className="current-turn">
+        {playerTurn === true ? (
+          <span>Player Turn</span>
+        ) : (
+          <span>Enemy Turn</span>
+        )}
+      </div>
+      <PlayerDisplay playerHealth={playerHealth} />
+      <EnemyDisplay enemyHealth={enemyHealth} />
     </div>
   );
 }
