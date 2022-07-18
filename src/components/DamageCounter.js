8b464c61dce998e48bb9c18681afe53d4d41d446
Cody Marshall <codymstuntsx@gmail.com>
2022-07-18 11:34:09 -0700

    damage counter component added

---
+++ src/components/DamageCounter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function DamageCounter() {
+return (
+    <div className='damage-container'>
+        <div className='damage-info'>
+            <span>1</span>
+        </div>
+    </div>
+    )
+
+}
+
+export default DamageCounter
