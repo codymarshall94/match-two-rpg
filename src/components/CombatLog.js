dda061722e02fffe07383f90424e78827a5d61a8
Cody Marshall <codymstuntsx@gmail.com>
2022-07-21 10:34:20 -0700

    removed cardmatchref and replaced for cardmatched

--- src/components/CombatLog.js
+++ src/components/CombatLog.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-
-function CombatLog({cardsCorrectRef, cards}) {
+function CombatLog({cardsMatched}) {
   let currentEffect;
-  if(cardsCorrectRef.current.length >= 1) {
-    currentEffect = cards[cardsCorrectRef.current.length - 1].effect || cards[cardsCorrectRef.current.length - 1].effect;
-  }
+ if(cardsMatched.length >= 1) {
+  currentEffect = cardsMatched[cardsMatched.length - 1].card.effect;
+ }
   return (
     <div className='log-box'>
         {currentEffect !== undefined ? <span className='combat-text'>{currentEffect}</span> : null}
     </div>
   )
 }
-
 //{cards[currentEffect].cardItem.card.effect.charAt(0).toUpperCase() + cards[currentEffect].cardItem.card.effect.slice(1)}
 
 export default CombatLog
