ea2daaa77408316c90abe9855ff2e317d6b81881
Cody Marshall <codymstuntsx@gmail.com>
2022-10-04 10:48:10 -0700

    added a disable card check

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -2,17 +2,33 @@ import React from "react";
 import "../css/cardgrid.css";
 import SingleCard from "./SingleCard";
 
-function CardGrid({ cards, handleCardClick, cardsFlipped, cardsMatched }) {
+function CardGrid({
+  cards,
+  handleCardClick,
+  cardsFlipped,
+  cardsMatched,
+  playerTurn,
+}) {
   return (
     <div className="container card-grid-container">
       <div className="row d-flex justify-content-center">
         {cards.map((card, index) => (
           <div
-            className="col-4 col-lg-5 card-container"
+            //apply disabled class if player turn is false
+            className={
+              cardsFlipped.length === 2 || playerTurn === false
+                ? "disabled-card col-4 col-lg-5 card-container "
+                : "col-4 col-lg-5 card-container"
+            }
             key={card.id}
-            onClick={() => handleCardClick({ card, index})}
+            onClick={() => handleCardClick({ card, index })}
           >
-            <SingleCard card={{card, index}} cardsFlipped={cardsFlipped} index={index} cardsMatched={cardsMatched}/>
+            <SingleCard
+              card={{ card, index }}
+              cardsFlipped={cardsFlipped}
+              index={index}
+              cardsMatched={cardsMatched}
+            />
           </div>
         ))}
       </div>
